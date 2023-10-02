5ac0f0d5730ee0726424d67aa55b347066673e59
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2023-10-02 10:24:10 +0700

    mod: docker for env and autoloader

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+use app\base\BaseController;
+
+spl_autoload_register(function ($className) {
+  if (!str_starts_with($className, "app\\")) {
+    return;
+  }
+
+  $className = substr($className, 4);
+  $className = str_replace("\\", "/", $className);
+  $class = __DIR__ . "/src/" . "{$className}.php";
+  include_once($class);
+});
