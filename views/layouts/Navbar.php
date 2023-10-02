9d1392afc1a0358a492dc8a8f2f3b0a046f45a9f
AustinPardosi <gabrielpardosi26@gmail.com>
2023-10-02 12:51:28 +0700

    feat: add navbar

---
+++ views/layouts/Navbar.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <link rel='stylesheet' href='/public/css/navbar.css'>
+</head>
+<body>
+    <nav class='navbar'>
+        <div class='logo' href='/'>
+            <img src='/public/assets/logo.svg' alt='logo' width="45" height="45" draggable='false'/>
+            <h1>Letterpaw</h1>
+        </div>
+        <ul class='nav-links'>
+            <div class='menu'>
+                <li><a href='/'>SIGN IN</a></li>
+                <li><a href='/'>CREATE ACCOUNT</a></li>
+                <li><a href='/'>FILMS</a></li>
+                <li><a href='/'>LISTS</a></li>
+                <li><a href='/'>MEMBERS</a></li>
+                <li><a href='/'>JOURNAL</a></li>
+            </div>
+        </ul>
+        <form method='get'>
+        <div class='input-search'>
+            <input class='' placeholder='Search...'/>
+            <img src='/public/assets/search-icon.svg' alt='search icon' />
+        </div>
+        </form>
+    </nav>
+</body>
+</html>
