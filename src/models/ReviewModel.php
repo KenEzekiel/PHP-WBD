37b64bb34f42b58bed27e400610dde37224e117e
AustinPardosi <gabrielpardosi26@gmail.com>
2023-10-02 18:35:40 +0700

    feat: add Review controller and repository

---
+++ src/models/ReviewModel.php
@@ -0,0 +1,36 @@
+<?php 
+
+namespace app\models;
+use app\base\BaseModel;
+
+class ReviewModel extends BaseModel {
+    public $user_id;
+    public $film_id;
+    public $rating;
+    public $notes;
+    public $published_time;
+
+    public function __construct() {
+        $this-> _primary_key = ['user_id', 'film_id'];
+        return $this;
+    }
+
+    public function constructFromArray($array) {
+        $this->user_id = $array['user_id'];
+        $this->film_id = $array['film_id'];
+        $this->rating = $array['rating'];
+        $this->notes = $array['notes'];
+        $this->published_time = $array['published_time'];
+        return $this;
+    }
+
+    public function toResponse() {
+        return array(
+            'user_id' => $this->user_id,
+            'film_id' => $this->film_id,
+            'rating' => $this->rating,
+            'notes' => $this->notes,
+            'published_time' => $this->published_time,
+        );
+    }
+}
