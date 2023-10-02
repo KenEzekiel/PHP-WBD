0ac48035e5664d9bc314b68d824deeb4c5476c86
AustinPardosi <gabrielpardosi26@gmail.com>
2023-10-02 18:36:12 +0700

    feat: add review services and controller

---
+++ src/controllers/ReviewController.php
@@ -0,0 +1,44 @@
+<?php 
+
+namespace app\controllers;
+
+use app\base\BaseController;
+use app\services\ReviewService;
+
+class ReviewController extends BaseController {
+    public function __construct() {
+        parent::__construct(null);
+    }
+
+    protected function get($urlParams) {
+        try {
+            parent::render($urlParams, 'give-review', 'layouts/base');
+        } catch (Exception $e) {
+            echo $e;
+        }
+    }
+
+    protected function post($urlParams) {
+        try {
+            parent::put($urlParams);
+        } catch (Exception $e) {
+            echo $e;
+        }
+    }
+
+    protected function put($urlParams) {
+        try {
+            parent::put($urlParams);
+        } catch (Exception $e) {
+            echo $e;
+        }
+    }
+
+    protected function delete($urlParams) {
+        try {
+            parent::delete($urlParams);
+        } catch (Exception $e) {
+            echo $e;
+        }
+    }
+}
---
+++ src/services/ReviewService.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace app\services;
+
+use app\base\BaseService;
+use app\exceptions\BadRequestException;
+use app\models\ReviewModel;
+use app\repositories\ReviewRepository;
+use PDO;
+
+class ReviewService extends BaseService {
+    protected static $instance;
+
+    private function __construct($repository) {
+        parent::__construct();
+        $this->repository = $repository;
+    }
+
+    public static function getInstance() {
+        if (!isset(self::$instance)) {
+            self::$instance = new static(
+                ReviewRepository::getInstance()
+            );
+        }
+        return self::$instance;
+    }
+
+    public function getAllReview() {
+        $allReview = $this->repository->findAll();
+        $reviews = [];
+        foreach ($allReview as $reviewData) {
+            $review = new ReviewModel();
+            $reviews[] = $review->constructFromArray($reviewData);
+        }
+        return $reviews;
+    }
+
+    public function create($user_id, $film_id, $rating, $notes, $published_time) {
+        $review = new ReviewModel();
+        $review->set('user_id', $user_id)->set('film_id', $film_id)->set('rating', $rating)->set('notes', $notes)->set('published_time', $published_time);
+
+        $response = $this->repository->getById($user_id, $film_id);
+        $reviewArray = $review->constructFromArray($response);
+
+        return $reviewArray;
+    }
+
+    // Wrapper get by from repository
+    public function getByRating($rating) {
+        $review = new ReviewModel();
+        $response = $this->repository->getByRating($rating);
+        if ($response) {
+            $review->constructFromArray($response);
+        }
+        return $review;
+    }
+
+    // Wrapper get by from repository
+    public function getByPublishedTime($published_time) {
+        $review = new ReviewModel();
+        $response = $this->repository->getByPublishedTime($published_time);
+        if ($response) {
+            $review->constructFromArray($response);
+        }
+        return $review;
+    }
+
+
+}
