46321812a7392977f03d03e35db1893dbfa4fdcb
Angela Livia Arumsari <16521177@mahasiswa.itb.ac.id>
2023-10-09 12:39:07 +0700

    feat: add styling for profile

--- views/layouts/base.php
+++ views/layouts/base.php
@@ -9,7 +9,8 @@
   <link rel='stylesheet' href='/public/css/home.css'>
   <link rel='stylesheet' href='/public/css/styles.css'>
   <link rel='stylesheet' href='/public/css/filmList.css'>
-  <link rel='stylesheet' href='/public/css/filmDetails.css'>
+  <link rel='stylesheet' href='/public/css/film-detail.css'>
+  <link rel='stylesheet' href='/public/css/profile.css'>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
