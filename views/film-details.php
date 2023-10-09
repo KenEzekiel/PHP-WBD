b3e4aa781e6c73d3a77bf038f570ef4aa408f65e
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2023-10-09 12:13:34 +0700

    feat: gateway for update film

--- views/film-details.php
+++ views/film-details.php
@@ -12,9 +12,19 @@
         <div class='card'>
             <div class='title-header'>
                 <h1 id="title"><?php echo $film->title ?></h1>
-                <button>
-                    <image id='fav' src="/public/assets/favorite.svg"></image>
-                </button>
+                <div class='right-btns'>
+                    <?
+                    if (isset($_SESSION['role']) && $_SESSION['role'] == 'admin') {
+                        echo "<a href='/update-film?film_id=$film->film_id '><button class='button'>Edit</button></a>";
+                    }
+                    ?>
+
+
+                    <button>
+                        <image id='fav' src="/public/assets/favorite.svg"></image>
+                    </button>
+                </div>
+
             </div>
 
             <h2><?php echo "$film->released_year | Directed by $film->director | $film->genre" ?></h2>
@@ -25,7 +35,7 @@
             <p><?php echo $film->description ?></p>
         </div>
 
-        <a id='link' href=<?= '/review?film_id=' . $film->film_id ?>>
+        <a id='link' href=<?= '/review?film_id=' . $film->film_id . "&isset=yes" ?>>
             <h1>Reviews ></h1>
         </a>
     </div>
