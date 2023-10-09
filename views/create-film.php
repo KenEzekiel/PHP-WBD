8258cb0095fe903ebf7660b7ec9abd682922669c
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2023-10-09 12:13:01 +0700

    fix: styling and placeholders

--- views/create-film.php
+++ views/create-film.php
@@ -7,32 +7,32 @@
     <div class="form-group">
       <label for="title">Title</label>
       <br>
-      <input class="input" type="text" id="title" name="title" required>
+      <input class="input" type="text" id="title" name="title" placeholder="Title" required>
     </div>
     <div class="form-group">
       <label for="released-year">Released Year</label>
       <br>
-      <input class="input" type="text" id="released-year" name="released-year" required>
+      <input class="input" type="text" id="released-year" name="released-year" placeholder="Released Year" required>
     </div>
     <div class="form-group">
       <label for="director">Director</label>
       <br>
-      <input class="input" type="text" id="director" name="director" required>
+      <input class="input" type="text" id="director" name="director" placeholder="Director" required>
     </div>
     <div class="form-group">
       <label for="description">Description</label>
       <br>
-      <input class="input" type="text" id="description" name="description" required>
+      <textarea class="input" type="text" id="description" name="description" placeholder="Description" required></textarea>
     </div>
     <div class="form-group">
       <label for="cast">Cast</label>
       <br>
-      <input class="input" type="text" id="cast" name="cast" required>
+      <input class="input" type="text" id="cast" name="cast" placeholder="Cast" required>
     </div>
     <div class="form-group">
       <label for="genre">Genre</label>
       <br>
-      <input class="input" type="text" id="genre" name="genre" required>
+      <input class="input" type="text" id="genre" name="genre" placeholder="Genre" required>
     </div>
     <div class="form-group">
       <label for="image-path">Image</label>
