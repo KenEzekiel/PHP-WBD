86d2d776e00bc5162c0a6c572047dc2040341489
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2023-11-15 14:02:13 +0700

    add: init db, still have to run script manually

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 docker compose up -d
-docker cp migrations/db/wbd.sql tubes-1-database:/wbd.sql
+docker cp script/init_db.sql tubes-1-database:/wbd.sql
 docker exec -it tubes-1-database mysql -u root -p
 use mysql_database;
 source wbd.sql;
-insert into user values(4, 'adm@email.com', 'adm', '$2y$10$gRlxyhscKHw7pB0IkgjxZOBVQStt2ZibucW5/RQVpQxuv4de8Bcm.', 'admin');
-# can now log in using username adm and password admin
 exit;
+
+# can now log in using username adm and password admin
+# and username user and password user
